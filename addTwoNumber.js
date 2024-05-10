39ba7ea4554616f9beb89dc4d4a68f53bec2f4eb
Siranjeevi619 <siranjeevi0619@gmail.com>
2024-05-11 00:58:09 +0530

    Unsolve problem

---
+++ addTwoNumber.js
@@ -0,0 +1,18 @@
+const l1 = [2,4,3]
+const l2 = [5,6,4]
+console.log(addTwoNumbers(l1,l2))
+
+function addTwoNumbers(l1, l2) {
+    newList = []
+    var arrayLength =   l1.length < l2.length ? l1.length : l2.length
+    for(let i=0;i<arrayLength;i++){
+        var addValue = l1[i]+l2[i]
+        if(addValue <10){
+            newList.push(addValue)
+        }
+        else{
+            newList.push(addValue%10)
+        }
+    }
+    return newList
+};
